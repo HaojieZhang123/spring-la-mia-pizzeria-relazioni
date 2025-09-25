5cda50815d21a28c4b6e33a35f2f1682c0b53c6d
HaojieZhang123 <haojiezhang123@gmail.com>
2025-09-25 22:16:46 +0200

    [ADD] Implemented IngredientController and created ingredients index view with Thymeleaf template

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controller/IngredientController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controller/IngredientController.java
@@ -3,6 +3,8 @@ package org.lessons.java.spring_la_mia_pizzeria_relazioni.controller;
 import org.lessons.java.spring_la_mia_pizzeria_relazioni.repository.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +13,12 @@ public class IngredientController {
 
     @Autowired
     private IngredientRepository ingredientRepository;
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("ingredients", ingredientRepository.findAll());
+
+        return "ingredients/index";
+    }
 
 }
