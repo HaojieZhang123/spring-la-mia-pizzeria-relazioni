8c502fca891cb10b585b5fea81bc6427e3c7323c
HaojieZhang123 <haojiezhang123@gmail.com>
2025-09-25 21:48:14 +0200

    [ADD] Introduced Ingredient model and established many-to-many relationship with Pizza

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/model/Pizza.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/model/Pizza.java
@@ -6,7 +6,10 @@ import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
@@ -38,6 +41,10 @@ public class Pizza {
 
     @OneToMany(mappedBy = "pizza")
     private List<Offer> offers;
+
+    @ManyToMany
+    @JoinTable(name = "ingredients_pizzas", joinColumns = @JoinColumn(name = "pizza_id"), inverseJoinColumns = @JoinColumn(name = "ingredient_id"))
+    private List<Ingredient> ingredients;
 
     // setters and getters
     public Integer getId() {
@@ -86,6 +93,14 @@ public class Pizza {
 
     public void setOffers(List<Offer> offers) {
         this.offers = offers;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
 }
